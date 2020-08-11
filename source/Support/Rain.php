9fc39b5404b59e6b1e793738e7d6594fdb91a15d
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-08-11 15:55:57 -0300

    adding cache and views

--- source/Support/Rain.php
+++ source/Support/Rain.php
@@ -2,11 +2,19 @@
 
 namespace Source\Support;
 
+use Rain\Tpl;
+
 class Rain
 {
     public function __construct()
     {
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."theme".DIRECTORY_SEPARATOR."views",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."cache",
+            "debug"         => true, // set to false to improve the speed
+        );
         
+        Tpl::configure( $config );
     }
 
     public function __destruct()
