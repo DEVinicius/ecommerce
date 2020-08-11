ae8834b2e221d21cd8d43b2707a0f50fd43e7972
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-08-11 15:59:25 -0300

    comment on Rain TPL

--- source/Support/Rain.php
+++ source/Support/Rain.php
@@ -6,15 +6,29 @@ use Rain\Tpl;
 
 class Rain
 {
+    /**
+     * @var
+     * Instance of Tpl
+     */
+    private $tpl;
+    
+    /**
+     * @var
+     * options array
+     */
+    private $defaults;
+
     public function __construct()
     {
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."theme".DIRECTORY_SEPARATOR."views",
-            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."cache",
-            "debug"         => true, // set to false to improve the speed
+            "tpl_dir"       => url("theme".DIRECTORY_SEPARATOR."views"),
+            "cache_dir"     => url("cache"),
+            "debug"         => false
         );
         
         Tpl::configure( $config );
+
+        $this->tpl = new Tpl();
     }
 
     public function __destruct()
